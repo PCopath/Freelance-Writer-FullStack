43db606d92935eac696919495fdc46c32092b6be
Yasir Balci <balci42Tr>
2025-04-12 04:04:34 +0300

    blog modeline image alanı eklendi

--- models/blog.js
+++ models/blog.js
@@ -1,18 +1,12 @@
 const mongoose = require("mongoose");
 
-const BlogSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const blogSchema = new mongoose.Schema(
+  {
+    title: String,
+    content: String,
+    image: String,
   },
-  content: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  { timestamps: true }
+);
 
-module.exports = mongoose.model("Blog", BlogSchema);
+module.exports = mongoose.model("Blog", blogSchema);
